e12d97f8269883d3e8a3a8b630d5ebb219a69113
Gabor Sornyei <info@sornyei.com>
2025-04-06 06:39:29 +0000

    Finish custom template tags

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 user_model= get_user_model()
+from blog.models import Post
 
 from django import template
 from django.utils.html import escape, mark_safe, format_html
@@ -31,3 +32,56 @@ def author_details(author, current_user):
     return format_html(f'{prefix}{name}{suffix}')
 
     return name
+
+
+@register.simple_tag
+def row(extra_classes=''):
+    return format_html(f'<div class="row {extra_classes}">')
+
+
+@register.simple_tag
+def endrow():
+    return format_html("</div>")
+
+
+@register.simple_tag
+def col(extra_classes=''):
+    return format_html(f'<div class="col {extra_classes}">')
+
+
+@register.simple_tag
+def endcol():
+    return format_html("</div>")
+
+
+@register.simple_tag(takes_context=True)
+def author_details_tag(context):
+    request = context["request"]
+    current_user = request.user
+    post = context["post"]
+    author = post.author
+
+    if author == current_user:
+        return format_html("<strong>me</strong>")
+
+    if author.first_name and author.last_name:
+        name = f"{author.first_name} {author.last_name}"
+    else:
+        name = f"{author.username}"
+
+    if author.email:
+        prefix = format_html('<a href="mailto:{}">', author.email)
+        suffix = format_html("</a>")
+    else:
+        prefix = ""
+        suffix = ""
+
+    return format_html("{}{}{}", prefix, name, suffix)
+
+
+@register.inclusion_tag("blog/post-list.html")
+def recent_posts(post):
+    posts = Post.objects.exclude(pk=post.pk)[:5]
+    return {"title": "Recent Posts", "posts": posts}
+
+
--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path("post/<slug>/", views.post_detail, name="blog-post-detail")
+
 ]
